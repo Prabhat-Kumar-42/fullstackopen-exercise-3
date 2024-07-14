54b8f6235b7e496b0e248b72219f98d91647bec1
blaze <prabhatkumarverma42@gmail.com>
2024-07-14 16:38:42 +0530

    module to generate large random number for id

---
+++ services/bigRandomNumber.js
@@ -0,0 +1,9 @@
+const getRandomInt = () => {
+  const min = 1000000;
+  const max = 9999999;
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+module.exports = {
+  getRandomInt,
+};
