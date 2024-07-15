4dd784facb6f308fa5d2f8117b7b83662bea11c7
blaze <prabhatkumarverma42@gmail.com>
2024-07-15 22:04:54 +0530

    Refactored Code into modules and added sloution for exercises 3.13-3.18

--- app.js
+++ app.js
@@ -1,124 +1,34 @@
 require("dotenv").config();
-const { DateTime } = require("luxon");
-const morgan = require("morgan");
-const { getRandomInt } = require("./services/bigRandomNumber");
 const express = require("express");
-
-// taking phonebook data from the specified file
-let phoneBook = require("./data/data.json");
+const personApiRouter = require("./routes/api/person.routes.js");
+const infoRouter = require("./routes/info.route.js");
 const { corsMiddleware } = require("./middeware/cors.middleware");
+const { morganMiddeware } = require("./middeware/morgan.middleware");
+const { connectMongo } = require("./db/mongo.db.js");
+const {
+  errorHandler,
+  unknownEndpoint,
+  castError,
+} = require("./middeware/errorHandler.middleware.js");
+
+connectMongo();
 
 const app = express();
 
+// Middlewares
 app.use(express.json());
 app.use(corsMiddleware);
 app.use(express.static("./dist/"));
+app.use(morganMiddeware);
 
-morgan.token("body", (req) =>
-  ["POST", "PUT"].includes(req.method) ? JSON.stringify(req.body) : null,
-);
-app.use(
-  morgan((tokens, req, res) => {
-    const body = tokens.body(req, res);
-    return [
-      tokens.method(req, res),
-      tokens.url(req, res),
-      tokens.status(req, res),
-      tokens.res(req, res, "content-length"),
-      "-",
-      tokens["response-time"](req, res),
-      "ms",
-      body ? body : "",
-    ]
-      .join(" ")
-      .trim();
-  }),
-);
+//Routes
+app.use("/api", personApiRouter);
+app.use("/info", infoRouter);
 
-app
-  .route("/api/persons")
-  .get((req, res) => {
-    return res.json(phoneBook);
-  })
-  .post((req, res) => {
-    const newPerson = req.body;
-    if (!newPerson) {
-      return res.status(400).json({ error: "Bad Request" });
-    }
-    if (!newPerson.name) {
-      return res.status(400).json({ error: "Name is Required Field" });
-    }
-    if (!newPerson.number) {
-      return res.status(400).json({ error: "Number is Required Field" });
-    }
-
-    const personExists = phoneBook.find(
-      (person) => person.name === newPerson.name,
-    );
-
-    if (personExists) {
-      return res.status(400).json({ error: "Name must be unique" });
-    }
-    const id = String(getRandomInt());
-    const name = newPerson.name;
-    const number = newPerson.number;
-    const newPersonData = {
-      id,
-      name,
-      number,
-    };
-    phoneBook.push(newPersonData);
-    return res.status(201).json(newPersonData);
-  });
-
-app
-  .route("/api/persons/:id")
-  .get((req, res) => {
-    const id = req.params.id;
-    const requestedPerson = phoneBook.find((person) => person.id === id);
-    if (!requestedPerson) {
-      return res.status(404).json({ error: "Not found" });
-    }
-    return res.json(requestedPerson);
-  })
-  .delete((req, res) => {
-    const id = req.params.id;
-    const requestedPerson = phoneBook.find((person) => person.id === id);
-    if (!requestedPerson) {
-      return res.status(404).json({ error: "Not found" });
-    }
-    phoneBook = phoneBook.filter((person) => person.id !== requestedPerson.id);
-    return res.json(requestedPerson);
-  })
-  .put((req, res) => {
-    if (!req.body) {
-      return res.status(404).json({ error: "Not found" });
-    }
-    const id = req.params.id;
-    const newNumber = req.body.number;
-    if (!id || !newNumber) {
-      return res.status(404).json({ error: "Not found" });
-    }
-    const requestedPersonIndex = phoneBook.findIndex(
-      (person) => person.id === id,
-    );
-    if (requestedPersonIndex == -1) {
-      return res.status(404).json({ error: "Not found" });
-    }
-    phoneBook[requestedPersonIndex].number = newNumber;
-    return res.json(phoneBook[requestedPersonIndex]);
-  });
-
-app.route("/info").get((req, res) => {
-  const date = DateTime.local().setZone(
-    Intl.DateTimeFormat().resolvedOptions().timeZone,
-  );
-  const formattedDate =
-    date.toFormat("EEE MMM dd yyyy HH:mm:ss 'GMT'ZZ") + ` (${date.zoneName})`;
-  const data = `<p>Phonebook has info for ${phoneBook.length} people</p>
-                <p>${formattedDate}</p>`;
-  return res.send(data);
-});
+//Error Handler
+app.use(unknownEndpoint);
+app.use(castError);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => console.log(`Server Started at port ${PORT}`));
